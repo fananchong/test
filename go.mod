14ba3eee0e77643060b58c9583e7e8185c09d758
fananchong <fananchong@xindong.com>
2020-09-10 16:59:59 +0800

    test metrics

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/fananchong/test
 
 go 1.14
 
-require github.com/prometheus/alertmanager v0.21.0
+require (
+	github.com/prometheus/alertmanager v0.21.0
+	github.com/prometheus/client_golang v1.6.0
+)
--- prometheus_test/metrics/main.go
+++ prometheus_test/metrics/main.go
@@ -1,1 +1,63 @@
 package main
+
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	myCounter   = prometheus.NewCounter(prometheus.CounterOpts{Name: "myCounter", Help: "Test Counter"})
+	myGauge     = prometheus.NewGauge(prometheus.GaugeOpts{Name: "myGauge", Help: "Test Gauge"})
+	mySummary   = prometheus.NewSummary(prometheus.SummaryOpts{Name: "mySummary", Help: "Test Summary", Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001}})
+	myHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{Name: "myHistogram", Help: "Test Histogram", Buckets: prometheus.LinearBuckets(95, 1, 10)})
+)
+
+func init() {
+	prometheus.MustRegister(myCounter, myGauge, mySummary, myHistogram)
+}
+
+func main() {
+
+	go func() {
+		for {
+			myCounter.Inc()
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			myGauge.Set(float64(100 + rand.Int31n(10) - 5))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			mySummary.Observe(float64(100 + rand.Int31n(10) - 5))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			myHistogram.Observe(float64(100 + rand.Int31n(10) - 5))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	// Expose the registered metrics via HTTP.
+	http.Handle("/metrics", promhttp.HandlerFor(
+		prometheus.DefaultGatherer,
+		promhttp.HandlerOpts{
+			// Opt into OpenMetrics to support exemplars.
+			EnableOpenMetrics: true,
+		},
+	))
+	log.Fatal(http.ListenAndServe(":8889", nil))
+}
