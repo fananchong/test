ff2412b1c15514bc1bfc0fb16da0e47034305201
fananchong <fananchong@126.com>
2020-09-15 00:15:40 +0800

    add test

--- struct_function_nil_test/main.go
+++ struct_function_nil_test/main.go
@@ -22,23 +22,15 @@ func test1() {
 }
 
 func test2() {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println(r)
-		}
-	}()
 	var a interface{} = nil
-	a.(*aa).F()
+	b, _ := a.(*aa) // 类型转化失败，则 b 为 nil , 类型为 aa
+	b.F()
 }
 
 func test3() {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println(r)
-		}
-	}()
 	var a ii
-	a.(*aa).F()
+	b, _ := a.(*aa) // 类型转化失败，则 b 为 nil , 类型为 aa
+	b.F()
 }
 
 func test4() {
@@ -53,8 +45,8 @@ func test5() {
 
 func main() {
 	test1() // aa is nil
-	test2() // interface conversion: interface {} is nil, not *main.aa
-	test3() // interface conversion: main.ii is nil, not *main.aa
+	test2() // aa is nil
+	test3() // aa is nil
 	test4() // aa is nil
 	test5() // aa is nil
 }
