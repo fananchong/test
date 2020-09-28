b4c0c265839b4723001949bbe2c515b7955b59fd
fananchong <fananchong@126.com>
2020-09-28 17:33:48 +0800

    Update setup.sh

--- prometheus_test/setup.sh
+++ prometheus_test/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CFG_DIR=$PWD/cfg
+DATA_DIR=$PWD/data
 echo $CFG_DIR
 
 docker rm -f prometheus
@@ -9,11 +10,14 @@ docker rm -f grafana
 docker rm -f alertmanager
 
 docker run -d --name node-exporter --restart=always -p 9100:9100 -v "/:/host:ro,rslave" quay.io/prometheus/node-exporter --path.rootfs=/host
-docker run -d --name grafana --restart=always -p 3000:3000 grafana/grafana
+docker run -d --name grafana --restart=always --user root -p 3000:3000 -v $DATA_DIR/grafana:/var/lib/grafana grafana/grafana
 docker run -d --name alertmanager --restart=always -p 9093:9093 -v $CFG_DIR:/etc/alertmanager prom/alertmanager
 
 docker run -d --name prometheus --restart=always \
-  -p 9090:9090 -v $CFG_DIR:/etc/prometheus \
+  --user root \
+  -p 9090:9090 \
+  -v $CFG_DIR:/etc/prometheus \
+  -v $DATA_DIR/prometheus:/prometheus \
   --link node-exporter \
   --link alertmanager \
   prom/prometheus:latest
