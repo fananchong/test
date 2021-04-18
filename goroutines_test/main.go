ce1634e33d352b172340030665a1dec62389de9c
fananchong <fananchong@xd.com>
2021-04-18 10:43:49 +0800

    up

--- goroutines_test/main.go
+++ goroutines_test/main.go
@@ -2,25 +2,116 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 	"sync/atomic"
 	"time"
 )
 
-var counter = int64(0)
-
-func main() {
-	N := 6397677
+func test_800b() {
+	N := 1000000
 	t1 := time.Now()
 	wait := &sync.WaitGroup{}
-	for i := 0; i < N; i++ {
+	for i := 0; i < N/100; i++ {
 		wait.Add(1)
 		go func() {
 			defer wait.Done()
-			atomic.AddInt64(&counter, 1)
+			var a [100]int64
+			for i := 0; i < 100; i++ {
+				atomic.AddInt64(&a[len(a)-i-1], 1)
+				runtime.Gosched()
+			}
 		}()
 	}
 	wait.Wait()
 	t2 := time.Now()
-	fmt.Printf("N=%d cost=%v avg=%v\n", N, t2.Sub(t1), t2.Sub(t1)/time.Duration(N))
+	fmt.Printf("800b N=%d cost=%v avg=%v\n", N, t2.Sub(t1), t2.Sub(t1)/time.Duration(N))
+}
+
+func test_8k() {
+	N := 1000000
+	t1 := time.Now()
+	wait := &sync.WaitGroup{}
+	for i := 0; i < N/100; i++ {
+		wait.Add(1)
+		go func() {
+			defer wait.Done()
+			var a [1024]int64
+			for i := 0; i < 100; i++ {
+				atomic.AddInt64(&a[len(a)-i-1], 1)
+				runtime.Gosched()
+			}
+		}()
+	}
+	wait.Wait()
+	t2 := time.Now()
+	fmt.Printf("8k N=%d cost=%v avg=%v\n", N, t2.Sub(t1), t2.Sub(t1)/time.Duration(N))
+}
+
+func test_80k() {
+	N := 1000000
+	t1 := time.Now()
+	wait := &sync.WaitGroup{}
+	for i := 0; i < N/100; i++ {
+		wait.Add(1)
+		go func() {
+			defer wait.Done()
+			var a [1024 * 10]int64
+			for i := 0; i < 100; i++ {
+				atomic.AddInt64(&a[len(a)-i-1], 1)
+				runtime.Gosched()
+			}
+		}()
+	}
+	wait.Wait()
+	t2 := time.Now()
+	fmt.Printf("80k N=%d cost=%v avg=%v\n", N, t2.Sub(t1), t2.Sub(t1)/time.Duration(N))
+}
+
+func test_800k() {
+	N := 1000000
+	t1 := time.Now()
+	wait := &sync.WaitGroup{}
+	for i := 0; i < N/100; i++ {
+		wait.Add(1)
+		go func() {
+			defer wait.Done()
+			var a [1024 * 100]int64
+			for i := 0; i < 100; i++ {
+				atomic.AddInt64(&a[len(a)-i-1], 1)
+				runtime.Gosched()
+			}
+		}()
+	}
+	wait.Wait()
+	t2 := time.Now()
+	fmt.Printf("800k N=%d cost=%v avg=%v\n", N, t2.Sub(t1), t2.Sub(t1)/time.Duration(N))
+}
+
+func test_4m() {
+	N := 1000000
+	t1 := time.Now()
+	wait := &sync.WaitGroup{}
+	for i := 0; i < N/100; i++ {
+		wait.Add(1)
+		go func() {
+			defer wait.Done()
+			var a [1024 * 500]int64
+			for i := 0; i < 100; i++ {
+				atomic.AddInt64(&a[len(a)-i-1], 1)
+				runtime.Gosched()
+			}
+		}()
+	}
+	wait.Wait()
+	t2 := time.Now()
+	fmt.Printf("4m N=%d cost=%v avg=%v\n", N, t2.Sub(t1), t2.Sub(t1)/time.Duration(N))
+}
+
+func main() {
+	test_800b()
+	test_8k()
+	test_80k()
+	test_800k()
+	test_4m()
 }
