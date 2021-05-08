9a6dca91303faff3a67e67acd0b329620e8ff20f
fananchong <fananchong@xd.com>
2021-05-08 17:25:58 +0800

    up

--- race/json.sh
+++ race/json.sh
@@ -6,15 +6,16 @@ msg="{\"Time\":\"${time}\", \"Msg\":\""
 while read line; do
     if [ "${line}" == "==================" ]; then
         i=$(expr $i + 1)
+    fi
+    if [ "$i" == "0" ]; then
+        echo ${line}
+    elif [ "$i" == "1" ]; then
         msg="${msg}${line}\n"
-        if [ "$i" == "2" ]; then
-            msg="${msg}\"}"
-            echo $msg
-            time=$(date "+%Y-%m-%d %H:%M:%S.000000")
-            i=0
-            msg="{\"Time\":\"${time}\", \"Msg\":\""
-        fi
-    else
-        msg="${msg}${line}\n"
+    elif [ "$i" == "2" ]; then
+        msg="${msg}\"}"
+        echo $msg
+        time=$(date "+%Y-%m-%d %H:%M:%S.000000")
+        i=0
+        msg="{\"Time\":\"${time}\", \"Msg\":\""
     fi
 done
