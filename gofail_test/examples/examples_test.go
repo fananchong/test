261cb028677bd9beb437246c5e45114be74a56e0
fananchong <fananchong@126.com>
2022-03-19 20:12:18 +0800

    gofail_test #2

--- gofail_test/examples/examples_test.go
+++ gofail_test/examples/examples_test.go
@@ -6,10 +6,34 @@ import (
 	gofail "github.com/etcd-io/gofail/runtime"
 )
 
-func TestWhatever(t *testing.T) {
+func TestExampleFunc(t *testing.T) {
 	gofail.Enable("examples/ExampleString", `return("testtesttest")`)
 	defer gofail.Disable("examples/ExampleString")
 	if ExampleFunc() != "testtesttest" {
+		t.Fatal("!!!")
+	}
+}
+
+func TestExampleOneLineFunc(t *testing.T) {
+	gofail.Enable("examples/ExampleOneLine", `return`)
+	defer gofail.Disable("examples/ExampleOneLine")
+	if ExampleOneLineFunc() != "abc" {
+		t.Fatal("!!!")
+	}
+}
+
+func TestExampleLabelsFunc(t *testing.T) {
+	gofail.Enable("examples/ExampleLabels", `return`)
+	defer gofail.Disable("examples/ExampleLabels")
+	if ExampleLabelsFunc() != "ijijijijij" {
+		t.Fatal("!!!")
+	}
+}
+
+func TestExampleLabelsGoFunc(t *testing.T) {
+	gofail.Enable("examples/ExampleLabelsGo", `return`)
+	defer gofail.Disable("examples/ExampleLabelsGo")
+	if ExampleLabelsGoFunc() != "ijijijijij" {
 		t.Fatal("!!!")
 	}
 }
