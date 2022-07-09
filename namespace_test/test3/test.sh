9a6f545e65928b7a8c701ba56b219752eb469301
fananchong <fananchong@126.com>
2022-07-09 13:05:07 +0000

    up

--- namespace_test/test3/test.sh
+++ namespace_test/test3/test.sh
@@ -17,8 +17,8 @@ brctl show
 
 # 创建 veth pair 网卡
 sudo ip link add veth0 type veth peer name veth1
-sudo ip addr add 1.2.3.101/24 dev veth0
-sudo ip addr add 1.2.3.102/24 dev veth1
+sudo ip addr add 192.168.1.201/24 dev veth0
+sudo ip addr add 192.168.1.202/24 dev veth1
 sudo ip link set veth0 up
 sudo ip link set veth1 up
 ip link
@@ -31,14 +31,19 @@ brctl show
 # shell2 另外控制台抓包
 sudo tcpdump -n -i veth1
 
-ping -c 1 -I veth0 1.2.3.102
+ping -c 1 -I veth0 192.168.1.202
 
 
-# ip 给网桥
-sudo ip addr del 1.2.3.101/24 dev veth0
-sudo ip addr add 1.2.3.101/24 dev br0
+# veth0 的 ip 给网桥
+sudo ip addr del 192.168.1.201/24 dev veth0
+sudo ip addr add 192.168.1.201/24 dev br0
 
 # shell2 另外控制台抓包
 sudo tcpdump -n -i veth1
 
-ping -c 1 -I br0 1.2.3.102
+ping -c 1 -I br0 192.168.1.202
+
+# 把物理网卡 eth0 给 br0
+sudo ip link set dev eth0 master br0
+bridge link
+brctl show
