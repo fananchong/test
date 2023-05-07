6b2eb115f04e151fc2a1c807f4e563be728a3d74
fananchong <fananchong@126.com>
2023-05-07 19:37:17 +0800

    go analysis #3

--- go_analysis_test/analysis.go
+++ go_analysis_test/analysis.go
@@ -35,36 +35,44 @@ func Analysis(path string, analyzer *analysis.Analyzer) error {
 }
 
 func GetCallGraphAnalyzer() *analysis.Analyzer {
+
+	func1 := func(pass *analysis.Pass, file *ast.File, node ast.Node) {
+		call, _ := node.(*ast.CallExpr)
+		if ident, ok := call.Fun.(*ast.Ident); ok && ident.Obj != nil {
+			pos := pass.Fset.Position(call.Pos())
+			line := pos.Line
+			var funcname string
+			for _, decl := range file.Decls {
+				funcDecl, ok := decl.(*ast.FuncDecl)
+				if !ok || funcDecl.Body == nil {
+					continue
+				}
+
+				posBegin := pass.Fset.Position(funcDecl.Body.Lbrace)
+				posEnd := pass.Fset.Position(funcDecl.Body.Rbrace)
+				if posBegin.Line > line || posEnd.Line < line {
+					continue
+				}
+				if posBegin.Filename != pos.Filename {
+					continue
+				}
+				funcname = funcDecl.Name.Name
+				// fmt.Printf("Function %s calls fmt.Println at line %d\n", funcDecl.Name.Name, line)
+				break
+			}
+
+			fmt.Printf("%s %s calls %s\n", pos, funcname, ident.Obj.Name)
+		}
+	}
+
 	f := func(pass *analysis.Pass) (interface{}, error) {
 		for _, file := range pass.Files {
 			ast.Inspect(file, func(node ast.Node) bool {
-				if call, ok := node.(*ast.CallExpr); ok {
-					if ident, ok := call.Fun.(*ast.Ident); ok && ident.Obj != nil {
-						pos := pass.Fset.Position(call.Pos())
-						line := pos.Line
-						var funcname string
-						for _, decl := range file.Decls {
-							funcDecl, ok := decl.(*ast.FuncDecl)
-							if !ok || funcDecl.Body == nil {
-								continue
-							}
-
-							posBegin := pass.Fset.Position(funcDecl.Body.Lbrace)
-							posEnd := pass.Fset.Position(funcDecl.Body.Rbrace)
-							if posBegin.Line > line || posEnd.Line < line {
-								continue
-							}
-							if posBegin.Filename != pos.Filename {
-								continue
-							}
-							funcname = funcDecl.Name.Name
-							// fmt.Printf("Function %s calls fmt.Println at line %d\n", funcDecl.Name.Name, line)
-							break
-						}
-
-						fmt.Printf("%s %s calls %s\n", pos, funcname, ident.Obj.Name)
-					}
+				switch node.(type) {
+				case *ast.CallExpr:
+					func1(pass, file, node)
 				}
+
 				return true
 			})
 		}
